b34cb079757990f5857bf32081ca46ab9dffd62b
kauii <kavishanwin8@hotmail.com>
2024-12-03 17:57:22 +0100

    implemented takeItem action

--- src/GameLoop.hs
+++ src/GameLoop.hs
@@ -4,7 +4,8 @@ import Types
 import Control.Monad.State
 import InitialState
 import Data.Maybe (fromMaybe)
-import Data.List (find)
+import Data.List (find, intercalate)
+import Data.Char (toLower)
 
 -- | Run the game loop using StateT to manage the game state
 runGameLoop :: GameState -> IO ()
@@ -17,9 +18,16 @@ gameLoop = do
     let player = playerState state
     let currentRoom = getPlayerRoom player (world state)
 
+     -- Extract the item names from the room
+    let itemNames = map itemName (items currentRoom)
+    let itemLine = if null itemNames
+                   then "There are no items in the room."
+                   else "The following items are in the room: " ++ intercalate ", " itemNames
+
     liftIO $ do
         putStrLn $ "You are in: " ++ roomName currentRoom
         putStrLn $ description currentRoom
+        putStrLn itemLine
         putStrLn "Available actions: (Go, Take, Drop, Inspect, Attack, TalkTo, OpenDoor, UseItem, Quit)"
 
     action <- liftIO getLine
@@ -39,7 +47,7 @@ parseAction :: String -> Maybe Action
 parseAction input =
     case words input of
         ["Go", dir] -> fmap Go (parseDirection dir)
-        ["Take", item]   -> Just (Take item)
+        ("Take" : itemWords)   -> Just (Take (unwords itemWords))
         ["Drop", item]   -> Just (Drop item)
         ["Inspect", name] -> Just (Inspect name)
         ["Attack", name] -> Just (Attack name)
@@ -85,10 +93,35 @@ movePlayer dir = do
             liftIO $ putStrLn $ "You move to " ++ roomName
         Nothing -> liftIO $ putStrLn "You can't go that way!"
 
+-- | Take item given item name
+takeItem :: String -> StateT GameState IO ()
+takeItem itemNameInput = do
+    state <- get
+    let player = playerState state
+    let currentRoom = getPlayerRoom player (world state)
+
+    -- Find the item in the current room
+    let maybeItem = find (\item -> map toLower (itemName item) == map toLower itemNameInput) (items currentRoom)
+
+    case maybeItem of
+        Just item -> do
+            -- Add the item to the player's inventory
+            let updatedInventory = item : inventory player
+            let updatedPlayer = player { inventory = updatedInventory }
+            
+            -- Remove the item from the room
+            let updatedRoomItems = filter (\i -> itemName i /= itemName item) (items currentRoom)
+            let updatedRoom = currentRoom { items = updatedRoomItems }
+            let updatedWorld = map (\room -> if roomName room == roomName currentRoom then updatedRoom else room) (world state)
+
+            -- Update the game state
+            put state { playerState = updatedPlayer, world = updatedWorld }
+            liftIO $ putStrLn $ "You picked up " ++ itemName item ++ "."
+        Nothing -> liftIO $ putStrLn $ "The item \"" ++ itemNameInput ++ "\" is not in this room."
+
 -- | Stub functions for other actions
-takeItem, dropItem, inspect, attackEnemy, talkTo, openDoor, useItem ::
+dropItem, inspect, attackEnemy, talkTo, openDoor, useItem ::
     String -> StateT GameState IO ()
-takeItem _ = liftIO $ putStrLn "Take action not implemented yet."
 dropItem _ = liftIO $ putStrLn "Drop action not implemented yet."
 inspect _ = liftIO $ putStrLn "Inspect action not implemented yet."
 attackEnemy _ = liftIO $ putStrLn "Attack action not implemented yet."
