495b1f4b8421e699bae8ec02975e343f8ad893f9
flolanz <florian.lanz@uzh.ch>
2024-12-06 18:33:35 +0100

    feat: check doors for navigation

--- src/GameLoop.hs
+++ src/GameLoop.hs
@@ -64,16 +64,41 @@ handleAction action = case action of
 -- | Move the player in a given direction
 movePlayer :: Direction -> StateT GameState IO ()
 movePlayer dir = do
-    state <- get
-    let player = playerState state
-    let currentRoom = getPlayerRoom player (world state)
+    gameState <- get
+    let player = playerState gameState
+    let currentRoom = getPlayerRoom player (world gameState)
     let maybeExit = lookup dir (exits currentRoom)
+    
     case maybeExit of
-        Just roomName -> do
-            let newRoom = findRoom roomName (world state)
-            put $ state { playerState = player { location = roomName } }
-            liftIO $ putStrLn $ "You move to " ++ roomName
+        Just exitRoomName -> do
+            let maybeDoor = find (\d -> leadsTo d == exitRoomName) (doors currentRoom)
+            case maybeDoor of
+                Just door -> do
+                    if isLocked door
+                        then case keyRequired door of
+                            Just keyName -> 
+                                if any (\item -> itemName item == keyName) (inventory player)
+                                then do
+                                    -- Unlock the door and remove the key
+                                    let newInventory = filter (\item -> itemName item /= keyName) (inventory player)
+                                    let updatedPlayer = player { inventory = newInventory }
+                                    let updatedDoor = door { isLocked = False }
+                                    let updatedDoors = map (\d -> if doorName d == doorName door then updatedDoor else d) (doors currentRoom)
+                                    let updatedRoom = currentRoom { doors = updatedDoors }
+                                    let updatedWorld = map (\room -> if roomName room == roomName currentRoom then updatedRoom else room) (world gameState)
+                                    put gameState { playerState = updatedPlayer, world = updatedWorld }
+                                    liftIO $ putStrLn $ "You unlocked the door with " ++ keyName ++ "."
+                                    movePlayer dir -- Retry moving after unlocking
+                                else
+                                    liftIO $ putStrLn "This path is currently blocked."
+                            Nothing -> liftIO $ putStrLn "This path is currently blocked."
+                        else do
+                            let newRoom = findRoom exitRoomName (world gameState)
+                            put $ gameState { playerState = player { location = exitRoomName } }
+                            liftIO $ putStrLn $ "You move to " ++ exitRoomName
+                Nothing -> liftIO $ putStrLn "You can't go that way!"
         Nothing -> liftIO $ putStrLn "You can't go that way!"
+
 
 -- | Take item given item name
 takeItem :: String -> StateT GameState IO ()
